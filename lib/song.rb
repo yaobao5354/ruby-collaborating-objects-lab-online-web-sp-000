213ea02d7719134e255f7d88b783e3b4912a3bc4
yaoyu bao <yaobao5354@gmail.com>
2019-09-03 20:17:58 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song 
+  attr_accessor :name
+  attr_reader :artist
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+  
+  def artist=(artist)
+    @artist = artist
+    artist.songs << self
+  end 
+  
+  def self.new_by_filename(filename)
+    binding.pry
+    self.new(filename)
+  end 
+  
+  def self.all
+    @@all
+  end 
+end 
